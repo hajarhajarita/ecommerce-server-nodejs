b6eb319ea1c565402b63a91bb90248596a6d5765
hajarhajarita <106028645+hajarhajarita@users.noreply.github.com>
2022-05-23 00:26:16 +0100

    initial commit

---
+++ data/Products.js
@@ -0,0 +1,104 @@
+const products = [
+  {
+    name: "safran",
+    image: "/images/6.png",
+    description:
+      "Le safran est une épice riche de calories si vous en consommez 100 g (352 Cal/100 g) ; or, la consommation est de l’ordre de la pincée donc à relativiser. De même, il est riche en minéraux, en oligo-éléments et en vitamines…pour 100 grammes",
+    price: 49,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "amande",
+    image: "/images/5.png",
+    description:
+      "amandes blanchies, se distinguent des amandes brutes par l'absence de peau brune. En bouche, les amandes émondées auront une saveur plus douce que les amandes brutes.",
+    price: 39,
+    countInStock: 10,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    name: "datte",
+    image: "/images/4.png",
+    description:
+      "une datte molle et sucrée, de grande taille, pe- sant entre 20 et 30 grammes, tandis que les autres dattes varient de 6 à 11 grammes. La récolte commence fin juillet-début août et se termine en octobre-novembre",
+    price: 29,
+    countInStock: 0,
+    rating: 3.5,
+    numReviews: 3,
+  },
+  {
+    name: "huile d'amande",
+    image: "/images/3.png",
+    description:
+      "L'huile d'amande douce hydrate et protège la peau et les cheveux, tout en leur redonnant de la vigueur et de l'éclat. Apaisante, elle soulage les irritations et les démangeaisons. Nourrissante, elle est parfaite en cas de gerçures ou de peaux desséchées, comme en prévention des vergetures",
+    price: 49,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 9,
+  },
+  {
+    name: "produit cosmetique des fleurs",
+    image: "/images/13.png",
+    description:
+      "Coffret de 4 cosmétiques traditionnels artisanaux, élaborés à partir d'eau de rose produite à Kelaat M'Gouna, au coeur de la Vallée des Roses dans le Sud du Maroc (région de Ouarzazate) : une eau florale de rose, une eau de parfum au mille fleurs, un savon liquide à la rose pour les mains et une lotion pour le corps à l'huile d'argan et aloé vera.",
+    price: 49,
+    countInStock: 7,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    name: "safran",
+    image: "/images/1.png",
+    description:
+      "Le safran est le nom que l'on donne à l'épice produit par la plante nommée Crocus Sativus L. Cette épice vient du prélèvement et du séchage des trois stigmates écarlates de Crocus Sativus L. On peut souvent retrouver ce dernier sous le nom de crocus de safranoignon de safran",
+    price: 29,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "datte",
+    image: "/images/8.png",
+    description:
+      "une datte molle et sucrée, de grande taille, pe- sant entre 20 et 30 grammes, tandis que les autres dattes varient de 6 à 11 grammes. La récolte commence fin juillet-début août et se termine en octobre-novembre",
+    price: 29,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "produits des roses",
+    image: "/images/12.png",
+    description:
+      "Vente produits à base de Roses locaaux et naturels de Kelaa Mgouna , des produits d’aromathérapie et de soins de santé de haute qualité Eau de Rose Savon de rose Huile de rose",
+    price: 39,
+    countInStock: 0,
+    rating: 3,
+    numReviews: 3,
+  },
+  {
+    name: "safran",
+    image: "/images/1.png",
+    description:
+      "Le safran est le nom que l'on donne à l'épice produit par la plante nommée Crocus Sativus L. Cette épice vient du prélèvement et du séchage des trois stigmates écarlates de Crocus Sativus L. On peut souvent retrouver ce dernier sous le nom de crocus de safranoignon de safran",
+    price: 29,
+    countInStock: 0,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    name: "rose",
+    image: "/images/11.png",
+    description:
+      "Vente produits à base de Roses locaaux et naturels de Kelaa Mgouna , des produits d’aromathérapie et de soins de santé de haute qualité Eau de Rose Savon de rose Huile de rose",
+    price: 19,
+    countInStock: 0,
+    rating: 1,
+    numReviews: 1,
+  },
+];
+
+export default products;
